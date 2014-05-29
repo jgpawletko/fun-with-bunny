4b541dff7f7bb6fd3a0e5aeaa68c59c0ec86def6
jgpawletko <jgpawletko@gmail.com>
2014-05-29 17:19:24 -0400

    add task generator, manual_ack:

---
+++ gen-tasks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./new_task.rb '0'
+./new_task.rb '1 .'
+./new_task.rb '2 ..'
+./new_task.rb '3 ...'
+./new_task.rb '4 ....'
+./new_task.rb '5 .....'
+./new_task.rb '6 ......'
+./new_task.rb '7 .......'
+./new_task.rb '8 ........'
+./new_task.rb '9 .........'
